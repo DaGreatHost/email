401815b2c14221bcabccd76417d8b31aa1ac1643
DaGreatHost <heherson230@gmail.com>
2025-04-24 20:02:47 +0800

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -18,3 +18,23 @@ def send_bulk_email(subject, html_body):
         with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
             server.login(SMTP_USER, SMTP_PASS)
             server.sendmail(SMTP_USER, email, msg.as_string())
+
+def send_confirmation_email(email):
+    subject = "Please confirm your email"
+    confirm_link = f"https://tgreward.shop/join.php?confirm={email}"
+    html_body = f"""
+        <h2>Welcome!</h2>
+        <p>Thanks for signing up. Please confirm your email by clicking the link below:</p>
+        <a href="{confirm_link}">Confirm My Email</a>
+        <br><br>
+        If you didn’t request this, you can ignore this email.
+    """ + unsubscribe_template.format(email=email)
+
+    msg = MIMEText(html_body, 'html')
+    msg["Subject"] = subject
+    msg["From"] = SMTP_USER
+    msg["To"] = email
+
+    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
+        server.login(SMTP_USER, SMTP_PASS)
+        server.sendmail(SMTP_USER, email, msg.as_string())
